5edd81010b913a0430772e412991e3aef2e545ea
kochurinn <kochurinn@yandex.ru>
2025-08-07 10:03:25 +0500

    vue-learn commit

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,5 +2,6 @@
   <div class="bg-black opacity-50 size-full absolute z-1 top-0 left-0"></div>
   <div class="bg-white w-120 absolute top-0 right-0 z-1 h-screen p-10">
     <h2 class="font-bold text-3xl">Корзина</h2>
+    <h2>Привет мир</h2>
   </div>
 </template>
