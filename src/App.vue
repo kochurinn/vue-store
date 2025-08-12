e1e42cd3eb9e0cb2e746e63c5837b34f8ff0ce9a
Никита Кочурин <kochurinn@yandex.ru>
2025-08-12 09:50:54 +0500

    vue-learn commit

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,24 @@ import Drawer from './components/Drawer.vue'
 
     <Header />
 
-    <h1 class="text-3xl font-bold mt-10">Все кроссовки</h1>
+    <div class="flex justify-between items-center mt-10">
+      <h1 class="text-3xl font-bold">Все кроссовки</h1>
+      <div class="flex">
+        <select class="py-2 px-3 border rounded-md outline-none mr-5">
+          <option>По названию</option>
+          <option>По возрастанию</option>
+          <option>По убыванию</option>
+        </select>
+        <div class="relative">
+          <img class="absolute left-4 top-3" src="/search.svg" alt="" />
+          <input
+            type="text"
+            placeholder="Поиск..."
+            class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400"
+          />
+        </div>
+      </div>
+    </div>
 
     <main class="mt-10">
       <CardList />
