14c2736b03b5ccf5bf69123295f165f0b0860574
Никита Кочурин <kochurinn@yandex.ru>
2025-08-14 09:35:52 +0500

    vue-learn commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
-import Drawer from './components/Drawer.vue'
+// import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://56036e980bcb4afb.mokky.dev/items')
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+watch(filters, async () => {
+  try {
+    const { data } = await axios.get(
+      'https://56036e980bcb4afb.mokky.dev/items?sortBy=' + filters.sortBy,
+    )
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+// watch(filters, async () => {
+//   try {
+//     const { data } = await axios.get(
+//       `https://56036e980bcb4afb.mokky.dev/items?title*${filters.searchQuery}*`,
+//     )
+//     items.value = data
+//   } catch (err) {
+//     console.log(err)
+//   }
+// })
 </script>
 
 <template>
   <body class="w-[1150px] m-auto">
-    <Drawer />
+    <!-- <Drawer /> -->
 
     <Header />
 
     <div class="flex justify-between items-center mt-10">
       <h1 class="text-3xl font-bold">Все кроссовки</h1>
       <div class="flex">
-        <select class="py-2 px-3 border rounded-md outline-none mr-5">
-          <option>По названию</option>
-          <option>По возрастанию</option>
-          <option>По убыванию</option>
+        <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none mr-5">
+          <option value="name">По названию</option>
+          <option value="price">По возрастанию</option>
+          <option value="-price">По убыванию</option>
         </select>
         <div class="relative">
           <img class="absolute left-4 top-3" src="/search.svg" alt="" />
           <input
+            @change="onChangeInput"
             type="text"
             placeholder="Поиск..."
             class="border rounded-md py-2 pl-10 pr-4 outline-none focus:border-gray-400"
@@ -30,7 +80,7 @@ import Drawer from './components/Drawer.vue'
     </div>
 
     <main class="mt-10">
-      <CardList />
+      <CardList :items="items" />
     </main>
   </body>
 </template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -3,8 +3,8 @@ import CartItemList from './CartItemList.vue'
 </script>
 
 <template>
-  <div class="bg-black opacity-50 size-full absolute z-1 top-0 left-0 hidden"></div>
-  <div class="bg-white w-120 absolute top-0 right-0 z-1 h-screen p-10 hidden">
+  <div class="bg-black opacity-50 size-full absolute z-1 top-0 left-0"></div>
+  <div class="bg-white w-120 absolute top-0 right-0 z-1 h-screen p-10">
     <h2 class="font-bold text-3xl mb-10">Корзина</h2>
 
     <CartItemList />
